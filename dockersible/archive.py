531c9f10e142fba9631a0b70321946b44e0429ef
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-16 07:35:48 +0000

    Adding tarfile member sanitization to extractall()

--- dockersible/archive.py
+++ dockersible/archive.py
@@ -17,7 +17,26 @@ def tar(source_dir, output_file):
 
 def untar(archive_file, output_dir):
     with tarfile.open(archive_file) as tar_file:
-        tar_file.extractall(os.path.abspath(output_dir))
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar_file, os.path.abspath(output_dir))
 
 
 def archive_volumes(docker_inspect, output_folder):
